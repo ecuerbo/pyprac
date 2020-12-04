507a0ea1d06b7e229a198e2f5722595d452acbd0
dies_irae <you@example.com>
2020-12-04 22:32:30 +0800

    started using visual code. all python practice will be staged from there.

---
+++ HelloWorld.py
@@ -0,0 +1,34 @@
+some_string = 'Hello world'
+print('Testing a string')
+print('-'*20)
+print('some_string', some string)
+print('some_string', some_string)
+print("some_string.startswith('H')", some_string.startswith('H'))
+print("some_string.startswith('h')", some_string.startswith('h'))
+print("some_string.startswith('x')", some_string.startswith('x'))
+
+some_string.endswith("d")
+print('some_string.endswith('s"))
+print(some_string.endswith("x"))
+x1 = 'elbon'
+x1.upper()
+'ELBON'
+x1.upper[2:4]
+print("some_string.istitle()", some_string.istitle())
+print('some_string.isupper()', some_string.isupper())
+print('some_string.isalpha()', some_string.isalpha)
+print('some_string.isalpha()', some_string.isalpha())
+print('String conversions')
+print('-' * 20)
+print('some_string.upper()', some_string.upper())
+print('some_string.lower()', some_string.lower())
+print('some_string.lower: ', some_string.lower())
+print('some_string.title(): ', some_string.title())
+print('some_string.swapcase(): ', some_string.swapcase())
+print('String leadin, trailing spaces', "      xyz      ".strip())
+some_string.lower().startswith('h')
+some_string.startswith('H')
+some_string.upper()
+some_string.startswith('G')
+some_string.endswith("D")
+
