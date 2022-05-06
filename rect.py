6ec25214588b44789e30660a402ca1eb40767af7
Elbon  Cuerbo <ecuerbo@Elbons-MacBook-Pro.local>
2022-05-06 13:54:51 +0800

    Trying again.LOLS

--- rect.py
+++ rect.py
@@ -1,3 +1,4 @@
+'''
 #Area of a rectangle
 def rect():
     length = int(input("Length: "))
@@ -8,3 +9,23 @@ def rect():
         area = length*width
         print("The area is ", area)
 rect()
+'''
+import sys
+def main():
+    if len(sys.argv) != 3:
+        exit("Needs 2 arguments: width length")
+
+    width = int(sys.argv[1])
+    length = int(sys.argv[2])
+
+    if length <= 0:
+        exit("length is not positive")
+
+    if width <= 0:
+        exit("width is not positive")
+
+    area = length * width
+    print('The area is ', area)
+
+
+main()
