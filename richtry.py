ed5563bb26cc31f7a188395c2f4df2f45232b62a
Elbon  Cuerbo <ecuerbo@Elbons-MacBook-Pro.local>
2022-05-06 15:37:48 +0800

    It worked?

---
+++ richtry.py
@@ -0,0 +1,13 @@
+from rich import print as rprint
+
+nums_list = [1,2,3,4]
+rprint(nums_list)
+
+nums_tuple = (1,2,3,4)
+rprint(nums_tuple)
+
+nums_dict = {'num_list': nums_list,'nums_tuple': nums_tuple}
+rprint(nums_dict)
+
+bool_list = [True, False]
+rprint(bool_list)
