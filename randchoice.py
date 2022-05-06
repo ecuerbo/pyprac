55d9174ca04c34c7fb33f01517386e704d536cd5
Elbon  Cuerbo <ecuerbo@Elbons-MacBook-Pro.local>
2022-05-06 13:44:08 +0800

    Trying again

---
+++ randchoice.py
@@ -0,0 +1,9 @@
+import random
+
+letter = "abcdefghijklmno"
+
+print(random.choice(letter)) # pick one of the letters
+
+fruits = ["Apple", "Banana", "Peach","Orange","Durian","Papaya"]
+
+print(random.choice(fruits)) #pic one of the fruits
