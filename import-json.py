a20840d7999fe4d7adea227dde86df3a5d705581
Elbon  Cuerbo <ecuerbo@Elbons-MacBook-Pro.local>
2022-05-07 13:27:45 +0800

    slight modification

--- import-json.py
+++ import-json.py
@@ -9,10 +9,10 @@ def get_content(user):
     """Extract text from user dict."""
     country = user["location"]["country"]
     name = f"{user['name']['first']}{user['name']['last']}"
-    return f"[b]{name}[/b]\n[yellow]{country}"
+    return f"[b]{name}[/b]\n[green]{country}"
 
 console = Console()
 
-users = json.loads(urlopen("https://randomuser.me/api/?results=30")).read()["results"]
+users = json.loads((urlopen("https://randomuser.me/api/?results=30")).read())["results"]
 user_renderables = [Panel(get_content(user), expand = True) for user in users]
 console.print(Columns(user_renderables))
