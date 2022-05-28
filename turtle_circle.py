a64b4ff7c584090c6d28610fa6f3ba2ab175f24d
Elbon <elbon.cuerbo@gmail.com>
2022-05-28 22:37:02 +0800

    before sleep coding

--- turtle_circle.py
+++ turtle_circle.py
@@ -11,5 +11,6 @@ def square(angle):
     x.forward(100)
     x.right(angle+10)
 
+
 for i in range(36):
-    square(90)                                                                                                                                                                                                                                                                                                                              )
+    square(45)                                                                                                                                                                                                                                                                                                                    )
